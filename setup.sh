1396b820b591bda88fef70d610e090367ae6e8bc
jjj <superp00t@tutanota.com>
2026-02-12 12:32:13 -0500

    chore: add files

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p deps
+CONFIG_LOCATION=$(pwd)
+
+is_service_installed() {
+  local service_name=$1
+  systemctl cat "$service_name" >/dev/null 2>&1
+}
+
+if ! is_service_installed "keyd.service"; then
+  echo "keyd is not installed";
+  git clone https://github.com/rvaiya/keyd deps/keyd.git
+  cd deps/keyd.git
+  make && sudo make install
+  sudo systemctl enable --now keyd
+fi
+cd $CONFIG_LOCATION
+
+echo "updating keyd config"
+sudo mv config/keyd/keyd.conf /etc/keyd/default.conf
+sudo systemctl restart keyd
+
