5402e72ee68ede42340aff9f394377f1e4a1e847
Phaneron <superp00t@ikrypto.club>
2026-02-13 00:00:09 +0000

    fix: atomize install parts; add color theme stuff

--- dotfiles/.vimrc
+++ dotfiles/.vimrc
@@ -1,1 +1,9 @@
 set number
+" Search down into subfolders
+" Provides tab-completion for all file-related tasks
+set path+=**
+
+" Display all matching files when we tab complete
+set wildmenu
+
+colorscheme base16-grayscale-dark
--- setup.sh
+++ setup.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-
 mkdir -p deps
-CONFIG_LOCATION=$(pwd)
+PATH=$PATH:$(pwd)/script
 
-is_service_installed() {
-  local service_name=$1
-  systemctl cat "$service_name" >/dev/null 2>&1
-}
-# install keyd
-# (among other things, makes Caps Lock the ESC key.)
-if ! is_service_installed "keyd.service"; then
-  echo "keyd is not installed";
-  git clone https://github.com/rvaiya/keyd deps/keyd.git
-  cd deps/keyd.git 
-  make && sudo make install
-  sudo systemctl enable --now keyd
+if is_service_installed "keyd.service"; then
+  echo "keyd is already installed"
 fi
-cd $CONFIG_LOCATION
-# update keyd config and restart
-echo "updating keyd config"
-sudo cp config/keyd/keyd.conf /etc/keyd/default.conf
-sudo systemctl restart keyd
+
+# install keyd
+install_keyd
+
 # install dotfiles
-cp -ar dotfiles/.* $HOME/
+install_dotfiles
