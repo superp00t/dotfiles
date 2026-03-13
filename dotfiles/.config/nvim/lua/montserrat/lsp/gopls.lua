d1f6128336e328a85d03d4aaaba6039f6bb81be4
superp00t <superp00t@tutanota.com>
2026-03-13 03:35:16 -0400

    fix(montserrat): add needed Go filetypes to fix Go LSP not loading when files are telescoped or netrw'd

--- dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
+++ dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
@@ -1,6 +1,7 @@
 vim.lsp.config('gopls', {
     cmd = { 'gopls' },
     root_markers = { 'go.mod', '.git' },
+	filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
     settings = {
         gopls = {
             analyses = {
