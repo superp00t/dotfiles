44028bc0c1237fb46736fb6b85a21d65011330b1
superp00t <superp00t@tutanota.com>
2026-03-17 06:52:31 +0000

    fix(montserrat): add missing nvim-cmp dependencies

--- dotfiles/.config/nvim/lua/montserrat/packages.lua
+++ dotfiles/.config/nvim/lua/montserrat/packages.lua
@@ -3,6 +3,9 @@ vim.pack.add({
 	{ src = 'https://github.com/nvim-lua/plenary.nvim',         commit = 'b9fd5226c2f76c951fc8ed5923d85e4de065e509' },
 	{ src = 'https://github.com/nvim-telescope/telescope.nvim', commit = '5255aa27c422de944791318024167ad5d40aad20' },
 	{ src = 'https://github.com/hrsh7th/cmp-nvim-lsp',          commit = 'cbc7b02bb99fae35cb42f514762b89b5126651ef' },
+	{ src = 'https://github.com/hrsh7th/cmp-buffer',            commit = 'b74fab3656eea9de20a9b8116afa3cfc4ec09657' },
+	{ src = 'https://github.com/hrsh7th/cmp-path',              commit = 'c642487086dbd9a93160e1679a1327be111cbc25' },
+	{ src = 'https://github.com/hrsh7th/cmp-cmdline',           commit = 'd126061b624e0af6c3a556428712dd4d4194ec6d' },
 	{ src = 'https://github.com/hrsh7th/nvim-cmp',              commit = 'da88697d7f45d16852c6b2769dc52387d1ddc45f' }
 })
 
