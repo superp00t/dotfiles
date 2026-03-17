c60baa4326abacf565328565d8cf2182fb7a81b7
superp00t <superp00t@tutanota.com>
2026-03-17 03:24:57 -0400

    fix(montserrat): enable cmp_nvim_lsp capabilities for gopls

--- dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
+++ dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
@@ -1,21 +1,21 @@
 vim.lsp.config('gopls', {
-    cmd = { 'gopls' },
-    root_markers = { 'go.mod', '.git' },
+	cmd = { 'gopls' },
+	root_markers = { 'go.mod', '.git' },
 	filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-            }
-        }
-    },
-	-- capabilities = require('cmp_nvim_lsp').default_capabilities()
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			}
+		}
+	},
+	capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = '*.go', 
-    callback = function()
-        vim.lsp.buf.format({
+	pattern = '*.go',
+	callback = function()
+		vim.lsp.buf.format({
 			async = false
 		})
 		vim.lsp.buf.code_action({
