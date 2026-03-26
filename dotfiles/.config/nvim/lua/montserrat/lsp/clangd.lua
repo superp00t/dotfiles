32b216245dcce7f9ef2c3981c628ce6e1146be98
superp00t <superp00t@tutanota.com>
2026-03-26 12:24:32 +0000

    tidy(montserrat): format & clean up unused code

--- dotfiles/.config/nvim/lua/montserrat/lsp/clangd.lua
+++ dotfiles/.config/nvim/lua/montserrat/lsp/clangd.lua
@@ -5,18 +5,18 @@ vim.lsp.config('clangd', {
     capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.m", "*.mm" },
+vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = { '*.c', '*.h', '*.cpp', '*.hpp', '*.m', '*.mm' },
     callback = function()
         -- Save the current cursor position to prevent jumping
         local view = vim.fn.winsaveview()
 
         -- Run clang-format on the current file path
         -- -i means "in-place" edit
-        vim.fn.system({ "clang-format", "-i", vim.fn.expand("%:p") })
+        vim.fn.system({ 'clang-format', '-i', vim.fn.expand('%:p') })
 
         -- Reload the file buffer to show changes
-        vim.cmd("edit!")
+        vim.cmd('edit!')
 
         -- Restore cursor position
         vim.fn.winrestview(view)
--- dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
+++ dotfiles/.config/nvim/lua/montserrat/lsp/gopls.lua
@@ -1,32 +1,32 @@
 vim.lsp.config('gopls', {
-	cmd = { 'gopls' },
-	root_markers = { 'go.mod', '.git' },
-	filetypes = { 'go', 'gomod' },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			}
-		}
-	},
-	capabilities = require('cmp_nvim_lsp').default_capabilities()
+    cmd = { 'gopls' },
+    root_markers = { 'go.mod', '.git' },
+    filetypes = { 'go', 'gomod' },
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+            }
+        }
+    },
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-	pattern = '*.go',
-	callback = function()
-		vim.lsp.buf.format({
-			async = false
-		})
-		vim.lsp.buf.code_action({
-			context = {
-				only = {
-					'source.organizeImports'
-				}
-			},
-			apply = true
-		})
-	end
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.format({
+            async = false
+        })
+        vim.lsp.buf.code_action({
+            context = {
+                only = {
+                    'source.organizeImports'
+                }
+            },
+            apply = true
+        })
+    end
 })
 
 vim.lsp.enable('gopls')
