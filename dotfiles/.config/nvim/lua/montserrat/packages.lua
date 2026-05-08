69234283f53db31c37f7feabb8d2db6e0cf3a4e5
superp00t <superp00t@tutanota.com>
2026-05-08 02:02:21 -0400

    chore(montserrat): upgrade some packages

--- dotfiles/.config/nvim/lua/montserrat/packages.lua
+++ dotfiles/.config/nvim/lua/montserrat/packages.lua
@@ -1,12 +1,12 @@
 vim.pack.add({
-	{ src = 'https://github.com/Mofiqul/vscode.nvim',                         commit = 'aa1102a7e15195c9cca22730b09224a7f7745ba8' },
-	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', commit = '93d60a475f0b08a8eceb99255863977d3a25f310' },
-	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter',             commit = 'e5f65e31a5ec0e8526b2311ff8c7aaea4a581c82' },
-	{ src = 'https://github.com/nvim-lua/plenary.nvim',                       commit = 'b9fd5226c2f76c951fc8ed5923d85e4de065e509' },
-	{ src = 'https://github.com/nvim-telescope/telescope.nvim',               commit = '5255aa27c422de944791318024167ad5d40aad20' },
-	{ src = 'https://github.com/hrsh7th/cmp-nvim-lsp',                        commit = 'cbc7b02bb99fae35cb42f514762b89b5126651ef' },
-	{ src = 'https://github.com/hrsh7th/cmp-buffer',                          commit = 'b74fab3656eea9de20a9b8116afa3cfc4ec09657' },
-	{ src = 'https://github.com/hrsh7th/cmp-path',                            commit = 'c642487086dbd9a93160e1679a1327be111cbc25' },
-	{ src = 'https://github.com/hrsh7th/cmp-cmdline',                         commit = 'd126061b624e0af6c3a556428712dd4d4194ec6d' },
-	{ src = 'https://github.com/hrsh7th/nvim-cmp',                            commit = 'da88697d7f45d16852c6b2769dc52387d1ddc45f' }
+    { src = 'https://github.com/Mofiqul/vscode.nvim',                         commit = 'aa1102a7e15195c9cca22730b09224a7f7745ba8' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', commit = '93d60a475f0b08a8eceb99255863977d3a25f310' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter',             commit = 'e5f65e31a5ec0e8526b2311ff8c7aaea4a581c82' },
+    { src = 'https://github.com/nvim-lua/plenary.nvim',                       commit = 'b9fd5226c2f76c951fc8ed5923d85e4de065e509' },
+    { src = 'https://github.com/nvim-telescope/telescope.nvim',               commit = 'f04ab730b8f9c6bf3f54a206d0dcddfd70c52d59' },
+    { src = 'https://github.com/hrsh7th/cmp-nvim-lsp',                        commit = 'cbc7b02bb99fae35cb42f514762b89b5126651ef' },
+    { src = 'https://github.com/hrsh7th/cmp-buffer',                          commit = 'b74fab3656eea9de20a9b8116afa3cfc4ec09657' },
+    { src = 'https://github.com/hrsh7th/cmp-path',                            commit = 'c642487086dbd9a93160e1679a1327be111cbc25' },
+    { src = 'https://github.com/hrsh7th/cmp-cmdline',                         commit = 'd126061b624e0af6c3a556428712dd4d4194ec6d' },
+    { src = 'https://github.com/hrsh7th/nvim-cmp',                            commit = 'a1d504892f2bc56c2e79b65c6faded2fd21f3eca' }
 })
